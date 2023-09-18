98c236cd336d985129ff5b018acfeda393fa1f4c
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-09-18 12:27:00 +0000

    index2

---
+++ index2.js
@@ -0,0 +1,28 @@
+"use strict";//use all js code as newer version
+// alert(3+3) we are uidng the node js ,not the browswer
+//
+
+console.log(3+3)
+//the bad usability of the code that is not good in practise
+
+console.log("aditya")
+
+
+let name= "aditya"
+let age=20
+let isLoggedIn=false
+//datatypes
+
+//number =>2 to power 53
+//bigint
+//string => double quotes ""
+//boolean => true or false
+//null => standalone value it is the representation for empty value
+//indefined => not assigned value till now
+//symbols=> to find the uniqueness
+//object=>
+//typeof =>to define the type of the object note:- type of null si an object and type of undefined is undefined
+
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
