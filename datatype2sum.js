a0c502448cf7ed41ce744f113fe7cd242c6505c8
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2023-09-21 05:30:27 +0000

    datatype

---
+++ datatype2sum.js
@@ -0,0 +1,37 @@
+//type of datatypes in javascript are seven //
+
+//string
+//boolean
+//number
+//bigInt
+//null
+//undefined
+//symbol
+
+//note:-all these seven datatypes stored under the stack memory
+
+//reference or derived datatypes are of three types
+//array
+//function
+//object
+
+const score=33
+console.log(typeof score);
+
+const score1=100.3
+console.log(typeof score1)
+
+const isloggedin=true
+const outsideTemp=null
+console.log(typeof isloggedin)
+
+//object example
+
+// const id=Symbol('123')
+// {
+//     const anotherid=Symbol('123')
+// }
+// console.log(id===anotherid)
+
+
+
